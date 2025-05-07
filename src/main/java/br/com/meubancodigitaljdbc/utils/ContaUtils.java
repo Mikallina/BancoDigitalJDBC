d1c3b773d71bc0e6d4a8ea2a35517e11760e7a7b
Mikallina <mi_borges@msn.coma>
2025-05-07 15:43:06 -0300

    Classes criadas. ContaUtils e TaxaService

---
+++ src/main/java/br/com/meubancodigitaljdbc/utils/ContaUtils.java
@@ -0,0 +1,30 @@
+package br.com.meubancodigitaljdbc.utils;
+
+import br.com.meubancodigitaljdbc.enuns.TipoConta;
+
+public class ContaUtils {
+
+    public String gerarNumeroConta(int agencia, TipoConta tipoConta) {
+        StringBuilder conta = new StringBuilder();
+        for (int i = 0; i < 7; i++) {
+            conta.append((int) (Math.random() * 10));
+        }
+
+        // Calcular o dígito verificador (8º número) usando módulo 11
+        int soma = 0;
+        for (int i = 0; i < conta.length(); i++) {
+            soma += (conta.charAt(i) - '0') * (i + 2);
+        }
+
+        int dv = soma % 11;
+        if (dv == 10) {
+            dv = 0;
+        }
+
+        conta.append(dv);
+
+        String tipoContaString = tipoConta.getTipoAbreviado();
+        return String.format("%s-%04d-%s", tipoContaString, agencia, conta.toString());
+    }
+
+}
