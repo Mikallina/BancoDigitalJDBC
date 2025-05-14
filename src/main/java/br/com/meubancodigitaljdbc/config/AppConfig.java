f4097f7657e23032d391c9ec1d35972f69ac8601
Mikallina <mi_borges@msn.coma>
2025-05-14 19:38:45 -0300

    AppConfig atualizado

--- src/main/java/br/com/meubancodigitaljdbc/config/AppConfig.java
+++ src/main/java/br/com/meubancodigitaljdbc/config/AppConfig.java
@@ -2,8 +2,9 @@ package br.com.meubancodigitaljdbc.config;
 
 import br.com.meubancodigitaljdbc.mapper.ContaRowMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
+@Configuration
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
