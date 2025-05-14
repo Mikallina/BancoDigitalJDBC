b52e21d34a0fd515fc51872c4d1cfe7a0b516f53
Mikallina <mi_borges@msn.coma>
2025-05-14 18:28:01 -0300

    Alteração DTO

--- src/main/java/br/com/meubancodigitaljdbc/dto/AlterarSenhaDTO.java
+++ src/main/java/br/com/meubancodigitaljdbc/dto/AlterarSenhaDTO.java
@@ -1,13 +1,13 @@
 package br.com.meubancodigitaljdbc.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+
 
 public class AlterarSenhaDTO {
-	@Schema(description = "CPF", example = "XXXXXXXXXXX")
+
     private String cpf;
-	@Schema(description = "Senha Antiga", example = "1234")
+
     private int senhaAntiga;
-	@Schema(description = "Nova Senha", example = "1235")
+
     private int senhaNova;
 	public AlterarSenhaDTO(String cpf, int senhaAntiga, int senhaNova) {
 		super();
